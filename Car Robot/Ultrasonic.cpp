cadb54ddc6da263137820bcb4ea3222fedc921f9
AbdallahHemdan <40190772+AbdallahHemdan@users.noreply.github.com>
2019-01-06 01:17:08 +0200

    Create Ultrasonic.cpp

---
+++ Car Robot/Ultrasonic.cpp
@@ -0,0 +1,34 @@
+// Ultrasonic.cpp - Library for HC-SR04 Ultrasonic Ranging Module.library
+
+#include "Arduino.h"
+#include "Ultrasonic.h"
+
+Ultrasonic::Ultrasonic(int TP, int EP)
+{
+   pinMode(TP,OUTPUT);
+   pinMode(EP,INPUT);
+   Trig_pin=TP;
+   Echo_pin=EP;
+}
+
+long Ultrasonic::Timing()
+{
+  digitalWrite(Trig_pin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(Trig_pin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(Trig_pin, LOW);
+  duration = pulseIn(Echo_pin,HIGH);
+  return duration;
+}
+
+long Ultrasonic::Ranging(int sys)
+{
+  Timing();
+  distacne_cm = duration /29 / 2 ;
+  distance_inc = duration / 74 / 2;
+  if (sys)
+  return distacne_cm;
+  else
+  return distance_inc;
+}
